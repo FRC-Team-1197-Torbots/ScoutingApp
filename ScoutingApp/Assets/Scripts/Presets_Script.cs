e14a218a33c580a47d7eb600c4262b197680c86f
SOLEHUNTER <31052505+SOLEHUNTER@users.noreply.github.com>
2018-02-09 18:57:50 -0800

    Added Regional and Year in New Version
    Made a presets Game Object that contains two public variables that when changed set the Year and Regional Number. It is in the new version so when you open it, it must be in Unity's version 2017.3.1f1.

--- ScoutingApp/Assets/Scripts/Presets_Script.cs
+++ ScoutingApp/Assets/Scripts/Presets_Script.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Presets_Script : MonoBehaviour {
-   // [Header("Presets")]
-   // public Text Year;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Presets")]
+    public int Year;
+    public Text YearText;
+    public Text YearText2;
+    public int Regional;
+    public Text RegionalText;
+    public Text RegionalText2;
+    private void Awake()
+    {
+        setPresets();
+        
+    }
+    public void setPresets()
+    {
+        if (YearText)
+        {
+            YearText.text = "Year: " + Year.ToString();
+        }
+        if (YearText2)
+        {
+            YearText2.text = "Year: " + Year.ToString();
+        }
+        if (RegionalText)
+        {
+            RegionalText.text = "Regional: " + Regional.ToString();
+        }
+        if (RegionalText2)
+        {
+            RegionalText2.text = "Regional: " + Regional.ToString();
+        }
+    }
 }
