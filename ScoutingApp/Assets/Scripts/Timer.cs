fc7bfef6cb2b3e4ce84f3c52ae86387a10b5ee11
Jeffrey Shen <jeffreyshen13@gmail.com>
2018-03-22 20:37:31 -0700

    added timer to auto panel

---
+++ ScoutingApp/Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public float timeLeft;
+    public Text time;
+    public GameObject AutoPanel;
+    public GameObject TelePanel;
+    bool pressed;
+
+    // Use this for initialization
+    void Awake () {
+        pressed = false;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        double test = (int)(timeLeft * 10) / 10.0;
+        time.GetComponent<Text>().text = test.ToString();
+        if (pressed == true) { 
+          if (timeLeft >= 0)
+           {
+              timeLeft -= Time.deltaTime;
+              Debug.Log(timeLeft);
+           }
+          if (timeLeft < 0)
+           {
+              AutoPanel.SetActive(false);
+              TelePanel.SetActive(true);
+              FindObjectOfType<MatchInfo>().TransitionToTele();
+           }
+         }
+    }
+
+    
+    public void startTimer()
+    {
+        pressed = true;
+           
+        
+    }
+}
